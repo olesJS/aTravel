f0c43ae1c59f9094b0a88ed74c5ac95015afdf52
Oleksii Yakymchuk <oles0305@gmail.com>
2023-08-28 11:20:37 +0300

    Initial Commit

---
+++ aTravel/aTravelApp.swift
@@ -0,0 +1,17 @@
+//
+//  aTravelApp.swift
+//  aTravel
+//
+//  Created by Олексій Якимчук on 28.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct aTravelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
