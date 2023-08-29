1798dd3c36188d409f2fc5f4030d4139257af422
Oleksii Yakymchuk <oles0305@gmail.com>
2023-08-29 15:01:58 +0300

    fm_1

---
+++ aTravel/AddView.swift
@@ -0,0 +1,59 @@
+//
+//  AddView.swift
+//  aTravel
+//
+//  Created by Олексій Якимчук on 28.08.2023.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    @EnvironmentObject var viewModel: ViewModel
+    @Environment(\.dismiss) var dismiss
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Add place")
+                    .font(.title.bold())
+                
+                Spacer()
+                
+                Button("Cancel") {
+                    dismiss()
+                }
+            }
+            .padding()
+            
+            Spacer()
+            
+            Form {
+                Section {
+                    TextField("Name", text: $viewModel.addName)
+                    VStack(alignment: .leading) {
+                        Text("Description:")
+                            .font(.callout.bold())
+                        TextEditor(text: $viewModel.addAbout)
+                            .frame(width: 300, height: 150)
+                    }
+                    DatePicker("Date of your trip:", selection: $viewModel.addDate, displayedComponents: .date)
+                    Picker("Type of your trip:", selection: $viewModel.addType) {
+                        ForEach(viewModel.tripTypes, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                }
+                
+                Section {
+                    if viewModel.addType == "Planned" {
+                        PlannedView()
+                            .environmentObject(viewModel)
+                    } else {
+                        VisitedView(viewModel: viewModel)
+                           
+                    }
+                }
+            }
+        }
+    }
+}
--- aTravel/ViewModel.swift
+++ aTravel/ViewModel.swift
@@ -8,10 +8,74 @@
 import Foundation
 import SwiftUI
 import MapKit
+import CoreData
 
 @MainActor class ViewModel: ObservableObject {
-    @Environment(\.managedObjectContext) var moc
     @FetchRequest(sortDescriptors: []) var items: FetchedResults<Item>
+    let moc: NSManagedObjectContext
+        
+    init(moc: NSManagedObjectContext) {
+        self.moc = moc
+    }
     
     @Published var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 50, longitude: 0), span: MKCoordinateSpan(latitudeDelta: 25, longitudeDelta: 25))
+    
+    @Published var isAddSheetActive = false
+    
+    let tripTypes = ["Visited", "Planned"]
+    
+    // AddView
+    @Published var addName: String = ""
+    @Published var addAbout: String = ""
+    @Published var addDate: Date = Date.now
+    @Published var addType: String = "Planned"
+    
+    // VisitedView
+    let maximumRating = 5
+    @Published var addRating = 3
+    
+    var isSaveButtonDisabled: Bool {
+        if addName.replacingOccurrences(of: " ", with: "").count == 0 { return true }
+        
+        return false
+    }
+    
+    // ImagePicker
+    @Published var isImagePickerActive = false
+    @Published var uiImage: UIImage?
+    
+    @Published var uiImageArray: [UIImage] = [UIImage]()
+    
+    func saveVisited() {
+        do {
+            let newPlace = Item(context: moc)
+            newPlace.id = UUID()
+            newPlace.name = addName
+            newPlace.about = addAbout
+            newPlace.date = addDate
+            newPlace.rating = Int16(addRating)
+            newPlace.type = addType
+            newPlace.latitude = mapRegion.center.latitude
+            newPlace.longitude = mapRegion.center.longitude
+            
+            var cdImages = [CDImage]()
+            
+            for img in uiImageArray {
+                let newCDImg = CDImage(context: moc)
+                newCDImg.name = addName
+                newCDImg.id = newPlace.id
+                newCDImg.img = img.jpegData(compressionQuality: 0.8)
+                cdImages.append(newCDImg)
+            }
+            
+            newPlace.addToImage(NSSet(array: cdImages))
+            try moc.save()
+            print("moc saved visited")
+            for i in items {
+                print(i.name ?? "ae")
+            }
+        } catch {
+            print("failed save")
+        }
+    }
 }
